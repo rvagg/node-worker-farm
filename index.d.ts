3bb54e766386097c906f23e01af1db17de8a0dfb
Dale Moore <daleamoore@gmail.com>
2018-03-26 19:37:10 +1100

    Updated definition file (#74)

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,33 @@
+import { ForkOptions } from "child_process";
+
+export = Farm;
+
+declare function Farm(name: string): Workers;
+declare function Farm(name: string, exportedMethods: string[]): Workers;
+declare function Farm(options: FarmOptions, name: string): Workers;
+declare function Farm(
+  options: FarmOptions,
+  name: string,
+  exportedMethods: string[],
+): Workers;
+
 interface Workers {
   (callback: WorkerCallback): void;
   (arg1: any, callback: WorkerCallback): void;
   (arg1: any, arg2: any, callback: WorkerCallback): void;
   (arg1: any, arg2: any, arg3: any, callback: WorkerCallback): void;
   (arg1: any, arg2: any, arg3: any, arg4: any, callback: WorkerCallback): void;
+}
+
+interface FarmOptions {
+  maxCallsPerWorker?: number;
+  maxConcurrentWorkers?: number;
+  maxConcurrentCallsPerWorker?: number;
+  maxConcurrentCalls?: number;
+  maxCallTime?: number;
+  maxRetries?: number;
+  autoStart?: boolean;
+  workerOptions?: ForkOptions;
 }
 
 type WorkerCallback =
@@ -19,26 +43,6 @@ type WorkerCallback2 = (arg1: any, arg2: any) => void;
 type WorkerCallback3 = (arg1: any, arg2: any, arg3: any) => void;
 type WorkerCallback4 = (arg1: any, arg2: any, arg3: any, arg4: any) => void;
 
-interface FarmOptions {
-  maxCallsPerWorker?: number
-  maxConcurrentWorkers?: number
-  maxConcurrentCallsPerWorker?: number
-  maxConcurrentCalls?: number
-  maxCallTime?: number
-  maxRetries?: number
-  autoStart?: boolean
-}
-
-interface WorkerFarm {
-  (name: string): Workers;
-  (name: string, exportedMethods: string[]): Workers;
-  (options: FarmOptions, name: string): Workers;
-  (options: FarmOptions, name: string, exportedMethods: string[]): Workers;
-
-  end: (workers: Workers) => void;
-}
-
-declare module "worker-farm" {
-  const workerFarm: WorkerFarm;
-  export = workerFarm;
+declare namespace Farm {
+  export function end(workers: Workers): void;
 }

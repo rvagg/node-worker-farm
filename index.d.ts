d32df7416aaf855ebed0cc57b4ed0efb3b5d8165
Dale Moore <dale@dmdigital.net.au>
2018-04-09 11:42:20 +1000

    Exported additional interfaces for passed in objects

--- index.d.ts
+++ index.d.ts
@@ -2,40 +2,14 @@ import { ForkOptions } from "child_process";
 
 export = Farm;
 
-declare function Farm(name: string): Workers;
-declare function Farm(name: string, exportedMethods: string[]): Workers;
-declare function Farm(options: FarmOptions, name: string): Workers;
+declare function Farm(name: string): Farm.Workers;
+declare function Farm(name: string, exportedMethods: string[]): Farm.Workers;
+declare function Farm(options: Farm.FarmOptions, name: string): Farm.Workers;
 declare function Farm(
-  options: FarmOptions,
+  options: Farm.FarmOptions,
   name: string,
   exportedMethods: string[],
-): Workers;
-
-interface Workers {
-  (callback: WorkerCallback): void;
-  (arg1: any, callback: WorkerCallback): void;
-  (arg1: any, arg2: any, callback: WorkerCallback): void;
-  (arg1: any, arg2: any, arg3: any, callback: WorkerCallback): void;
-  (arg1: any, arg2: any, arg3: any, arg4: any, callback: WorkerCallback): void;
-}
-
-interface FarmOptions {
-  maxCallsPerWorker?: number;
-  maxConcurrentWorkers?: number;
-  maxConcurrentCallsPerWorker?: number;
-  maxConcurrentCalls?: number;
-  maxCallTime?: number;
-  maxRetries?: number;
-  autoStart?: boolean;
-  workerOptions?: ForkOptions;
-}
-
-type WorkerCallback =
-  | WorkerCallback0
-  | WorkerCallback1
-  | WorkerCallback2
-  | WorkerCallback3
-  | WorkerCallback4;
+): Farm.Workers;
 
 type WorkerCallback0 = () => void;
 type WorkerCallback1 = (arg1: any) => void;
@@ -45,4 +19,36 @@ type WorkerCallback4 = (arg1: any, arg2: any, arg3: any, arg4: any) => void;
 
 declare namespace Farm {
   export function end(workers: Workers): void;
+
+  export interface Workers {
+    (callback: WorkerCallback): void;
+    (arg1: any, callback: WorkerCallback): void;
+    (arg1: any, arg2: any, callback: WorkerCallback): void;
+    (arg1: any, arg2: any, arg3: any, callback: WorkerCallback): void;
+    (
+      arg1: any,
+      arg2: any,
+      arg3: any,
+      arg4: any,
+      callback: WorkerCallback,
+    ): void;
+  }
+
+  export interface FarmOptions {
+    maxCallsPerWorker?: number;
+    maxConcurrentWorkers?: number;
+    maxConcurrentCallsPerWorker?: number;
+    maxConcurrentCalls?: number;
+    maxCallTime?: number;
+    maxRetries?: number;
+    autoStart?: boolean;
+    workerOptions?: ForkOptions;
+  }
+
+  export type WorkerCallback =
+    | WorkerCallback0
+    | WorkerCallback1
+    | WorkerCallback2
+    | WorkerCallback3
+    | WorkerCallback4;
 }
